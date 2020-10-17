bcb78586e077a2b8c10d97e7ff979934f364c2d8
mkusaka <hinoshita1992@gmail.com>
2020-10-17 16:24:35 +0900

    builder pattern

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,31 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("My RPN program")
+        .version("0.0.1")
+        .author("your name")
+        .about("Super awesome sample RPN calc")
+        .arg(
+            Arg::new("formula_file")
+                .about("formulars written in RPN")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .about("Sets the level of verbosity")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("no file specified."),
+    }
+
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
